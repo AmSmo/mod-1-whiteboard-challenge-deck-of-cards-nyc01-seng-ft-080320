61d6fc58dd5323b106e705cc8ca1a5e6e74a9a04
AmSmo <adam.smolenski@gmail.com>
2020-08-05 00:06:29 -0400

    One way... there are others

--- solution.rb
+++ solution.rb
@@ -1,7 +1,38 @@
+require 'pry'
 class Deck
+    attr_accessor :cards
+    def initialize()
+        @cards = Card.shuffle
+    end
+
+    def choose_card
+        @cards.delete(@cards.sample)
+    end
 
 end
 
 class Card
+
+    SUITS= %w(Hearts Clubs Spades Diamonds)
+
+    RANKS = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+    attr_reader :rank, :suit
+   
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
+    
+    def self.shuffle
+        full_deck = []
+        RANKS.each do |rank|
+            SUITS.each do |suit|
+                
+                full_deck << Card.new(suit,rank)
+            end
+        end
+        full_deck
+       
+    end
 
 end
